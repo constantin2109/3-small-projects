ddf49709fdb1673abd90d94d5ab4db8daf6804a8
Constantin Bagrin <constantinbagrin@gmail.com>
2023-03-23 11:11:56 +0000

    Update app.js

--- project 1/app.js
+++ project 1/app.js
@@ -14,3 +14,23 @@ openFace.addEventListener('click', () => {
         
     }
 });
+const emoji = document.querySelectorAll(".emoji")
+emoji.forEach((emoji) => {
+   emoji.addEventListener("click", () => {
+      const color = getRandomColor()
+      emoji.style.backgroundColor = color
+      document.body.style.background = color
+      document.querySelectorAll("*").forEach((el) => {
+         el.style.backgroundColor = color
+      })
+   })
+})
+
+function getRandomColor() {
+   const letters = "0123456789ABCDEF"
+   let color = "#"
+   for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)]
+   }
+   return color
+}
